44e5323ac2e8759e2465d5d01b916727380b1e10
t-ozawa_hg <t-ozawa@hg.locopo-jp.com>
2020-04-23 17:54:26 +0900

    エラー修正

--- Apigee_ds/Apigee_ds/routes/ds_documents.js
+++ Apigee_ds/Apigee_ds/routes/ds_documents.js
@@ -1,11 +1,26 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
+
 var { Client } = require('pg');
+
+const log4js = require('log4js')
+
+log4js.configure({
+    appenders: {
+        system: { type: 'file', filename: 'system.log' }
+    },
+    categories: {
+        default: { appenders: ['system'], level: 'debug' },
+    }
+});
+const logger = log4js.getLogger('system');
 
 /* GET docuign document. */
 router.get('/:document_id', function (req, res) {
-    var id = req.params.ds_document_id;
+
+    var id = req.params.document_id;
+    logger.debug('ds_document_id:' + id);
 
     var client = new Client({
         user: 'postgres',
@@ -33,13 +48,13 @@ router.get('/:document_id', function (req, res) {
                 default:
                     res.json({
                         status_code: '400',
-                        message: 'Invalid parameter.'
+                        message: 'invalid parameter.'
                     })
             }
         });
     }
 
-    const select = {
+    var select = {
         text: 'SELECT * FROM docusign_document WHERE ds_document_id = $1',
         values: [id]
     };
@@ -60,23 +75,23 @@ router.get('/:document_id', function (req, res) {
                 fields.forEach(function (field, i) {
                     switch (field) {
                         case 'ds_envelope_id':
-                            json.push({ ds_envelope_id: result.rows[0].ds_envelope_id });
+                            json.ds_envelope_id = result.rows[0].ds_envelope_id;
                             break;
                         case 'process_status':
-                            json.push({ process_status: result.rows[0].process_status });
+                            json.process_status = result.rows[0].process_status;
                             break;
                         case 'contract_end_date':
-                            json.push({ contract_end_date: result.rows[0].contract_end_date });
+                            json.contract_end_date = result.rows[0].contract_end_date;
                             break;
                         case 'registration_date':
-                            json.push({ registration_date: result.rows[0].registration_date });
+                            json.registration_date = result.rows[0].registration_date;
                             break;
                     };
                 });
             };
             res.json(json);
         }
-    }
-})
+    });
+});
 
 module.exports = router;
--- Apigee_ds/Apigee_ds/routes/test.js
+++ Apigee_ds/Apigee_ds/routes/test.js
@@ -1,30 +1,51 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
+var { Client } = require('pg');
+
 
 /* GET users listing. */
-router.get('/', function (req, res) {
-    if (req.query.fields) {
-        const fields = req.query.fields.split(',');
-        fields.forEach(function (field, i) {
-            switch (field) {
-                case 'aaa':
-                    res.json({
-                        status_code: '200',
-                        message: 'OK'
-                    })
-                default:
-                    res.json({
-                        status_code: '400',
-                        message: 'Invalid parameter.' })
-            }
-        });
+router.get('/:document_id', function (req, res) {
+    var id = req.params.document_id;
 
-    } else {
-        res.json({
-            status_code: '200',
-            message:'OK'
-        })
-    }
+    var client = new Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'docusign_test',
+        password: 'root',
+        port: 5432
+    });
+
+    client.connect();
+
+    client.query('SET search_path = docusign_test', (err, result) => {
+        if (err) {
+            res.json({
+                message: 'SET search_path Error.'
+            });
+        }
+    });
+
+    const select = {
+        text: 'SELECT * FROM docusign_document WHERE ds_document_id = $1',
+        values: [id]
+    };
+
+    client.query(select, (err, result) => {
+        if (err) {
+            res.json({
+                message: 'Select Error.'
+            });
+        } else {
+            res.json({
+                ds_document_id: result.rows[0].ds_document_id,
+                ds_document_name: result.rows[0].ds_document_name,
+                ds_status: result.rows[0].ds_status
+            });
+        }
+    });
+
+
+
 });
     module.exports = router;
